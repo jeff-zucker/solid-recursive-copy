aa7dbfedea7bf4c9c00f6ff4c34c5898710febcf
Dub Zed <dubzed@gmail.com>
2019-01-18 15:51:54 -0800

    test fixes

--- tests/browserless.js
+++ tests/browserless.js
@@ -2,13 +2,17 @@
 const auth = require('solid-auth-cli')
 const deepCopy = require('../src/recurse.js')
 
-const source  = "https://jeffz.solid.community/public/solidsite2"
-const target  = "https://jeffz.solid.community/public/solidsite"
+if( process.argv.length < 4 ) {
+    console.log("you must enter directories to copy from and to");
+    process.exit(-1)
+}
+const here  = process.argv[2]
+const there = process.argv[3]
 
 console.log(`logging in`)
 auth.login().then( session => {
     console.log(`logged in as <${session.webId}>`)
-        deepCopy( source, target ).then( res => {
+        deepCopy( here, there ).then( res => {
     },e => console.log("Error copying : "+e))
 },e => console.log("Error logging in : "+e))
 
